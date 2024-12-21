2f463d50abb8cd1a8c5a2efac74b5eedd483cb20
Muhammad Ali Shah <muhammad.ali@gmail.com>
2024-12-21 19:09:19 +0400

    Spring Initializr used to create the project with gradle build

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # open-banking-manning
 Covers manning live project
+
+## Define the Contract
+The first part of the live project is defining the contract for better-banking.io, a FinTech startup.
---
+++ define-the-contract/tp/src/test/java/io/betterbanking/tp/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.betterbanking.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
