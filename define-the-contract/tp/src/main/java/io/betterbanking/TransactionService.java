c860f3611845c856d1ca247208ca05f90cefa4b6
Muhammad Ali Shah <muhammad.ali@gmail.com>
2024-12-21 19:31:09 +0400

    Created TransactionService to fetch transactions list based on the account number

---
+++ define-the-contract/tp/src/main/java/io/betterbanking/TransactionService.java
@@ -0,0 +1,21 @@
+package io.betterbanking;
+
+import io.betterbanking.tp.Transaction;
+
+import java.util.List;
+import java.util.LinkedList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    
+    /**
+     * Fetch transactions list by account number
+     * @param accountNumber
+     * @return
+     */
+    public List<Transaction> findAllByAccountNumber (String accountNumber) {
+        List<Transaction> list = new LinkedList<>();
+        return list;
+    }
+}
