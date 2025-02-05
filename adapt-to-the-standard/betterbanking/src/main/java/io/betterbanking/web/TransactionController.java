b0dfdcae890736fcb1b2d694a1ace803c8be2991
Muhammad Ali Shah <muhammad.ali@gmail.com>
2025-02-05 12:40:11 +0000

    Added mock test cases

--- adapt-to-the-standard/betterbanking/src/main/java/io/betterbanking/web/TransactionController.java
+++ adapt-to-the-standard/betterbanking/src/main/java/io/betterbanking/web/TransactionController.java
@@ -2,9 +2,11 @@ package io.betterbanking.web;
 
 import io.betterbanking.entity.Transaction;
 import io.betterbanking.service.TransactionService;
+import io.betterbanking.web.dto.TransactionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,7 +17,25 @@ public class TransactionController {
     TransactionService svc;
 
     @GetMapping("/{acctNr}")
-    public List<Transaction> findAllByAccountNumber (@PathVariable final Integer acctNr) {
-        return svc.findAllByAccountNumber(acctNr);
+    public List<TransactionDto> findAllByAccountNumber (@PathVariable final Integer acctNr) {
+        List<Transaction> list = svc.findAllByAccountNumber(acctNr);
+        List<TransactionDto> listDto = new ArrayList<>(list.size());
+
+        for (Transaction txn : list) {
+            TransactionDto dto = buildTransactionDto (txn);
+            listDto.add(dto);
+        }
+
+        return listDto;
+    }
+
+    private TransactionDto buildTransactionDto (Transaction txn) {
+        TransactionDto dto = TransactionDto.builder()
+                .accountNumber(txn.getAccountNumber())
+                .currency(txn.getCurrency())
+                .amount(txn.getAmount())
+                .build();
+
+        return dto;
     }
 }
---
+++ adapt-to-the-standard/betterbanking/src/test/java/io/betterbanking/service/TransactionServiceTest.java
@@ -0,0 +1,64 @@
+package io.betterbanking.service;
+
+import io.betterbanking.entity.Transaction;
+import io.betterbanking.repository.TransactionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * Based on @MockitoBean TransactionRepository, test TransactionService
+ */
+@SpringBootTest
+public class TransactionServiceTest {
+
+    @Autowired
+    private TransactionService svc;
+
+    @MockitoBean
+    private  TransactionRepository repo;
+
+    @Test
+    public void shouldReturnEmptyListWhenAccountNumberNotFound () {
+        final Integer ACCOUNT_NUMBER = 999;
+        when (repo.findAllByAccountNumber(ACCOUNT_NUMBER)).thenReturn(List.of());
+
+        List<Transaction> list = svc.findAllByAccountNumber(ACCOUNT_NUMBER);
+        assertTrue (list.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnTransactionsWhenAccountNumberFound() {
+        final Integer ACCOUNT_NUMBER = 123;
+
+        List<Transaction> list = new ArrayList<>();
+        list.add (buildTransaction(ACCOUNT_NUMBER, "GBP", 35.0));
+        list.add (buildTransaction(ACCOUNT_NUMBER, "GBP", 135.0));
+
+        when(repo.findAllByAccountNumber(ACCOUNT_NUMBER)).thenReturn(list);
+
+        List<Transaction> txns = svc.findAllByAccountNumber(ACCOUNT_NUMBER);
+        assertFalse (txns.isEmpty());
+        assertEquals (ACCOUNT_NUMBER, txns.get(0).getAccountNumber());
+    }
+
+     public Transaction buildTransaction(Integer acctNr, String curr, Double amt) {
+         Transaction txn = Transaction.builder()
+                 .accountNumber(acctNr)
+                 .amount(amt)
+                 .currency(curr)
+                 .date (new Date())
+                 .build();
+
+         return txn;
+     }
+}
