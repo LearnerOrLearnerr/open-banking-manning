49d8d1ed52105e774d69c0ae0202082d89f8a053
Muhammad Ali Shah <muhammad.ali@gmail.com>
2025-02-09 18:09:08 +0000

    Fixed resilience4j using Spring AOP; added TransactionRepository source as fallback

--- adapt-to-the-standard/betterbanking/src/main/java/io/betterbanking/service/TransactionService.java
+++ adapt-to-the-standard/betterbanking/src/main/java/io/betterbanking/service/TransactionService.java
@@ -2,6 +2,7 @@ package io.betterbanking.service;
 
 import io.betterbanking.entity.Transaction;
 import io.betterbanking.repository.TransactionApiClient;
+import io.betterbanking.repository.TransactionRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,11 @@ public class TransactionService {
     @Autowired
     TransactionApiClient apiClient;
 
-    @CircuitBreaker(name = "transactionService",fallbackMethod = "foundNone")
-    public List<Transaction> findAllByAccountNumber(final Integer acctNr) {
+    @Autowired
+    TransactionRepository repo;
+
+    @CircuitBreaker(name = "transactionService", fallbackMethod = "foundNone")
+    public List<Transaction> findAllByAccountNumber(final Integer acctNr){
         List<Transaction> list = apiClient.findAllByAccountNumber(acctNr);
 
         for (Transaction t : list) {
@@ -26,6 +30,7 @@ public class TransactionService {
     }
 
     public List<Transaction> foundNone(final Integer acctNr, Throwable t) {
-        return List.of();
+        List<Transaction> list = repo.findAllByAccountNumber(acctNr);
+        return list;
     }
 }
